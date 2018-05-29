cac2aad8ceda9f82babbcfc303c0f2d4f53bc5a7
zhengxiaoyong <zhengxiaoyong@weidian.com>
2018-05-29 10:09:58 +0800

    commit.

---
+++ OpenJDK-v10_0_1/langtools/src/jdk.javadoc/share/classes/jdk/javadoc/internal/tool/resources/version.java
@@ -0,0 +1,11 @@
+package jdk.javadoc.internal.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "10.0.1+10" },
+            { "jdk", "10.0.1" },
+            { "release", "10.0.1" },
+        };
+    }
+}
---
+++ OpenJDK-v10_0_1/langtools/src/jdk.jdeps/share/classes/com/sun/tools/javap/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javap.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "10.0.1+10" },
+            { "jdk", "10.0.1" },
+            { "release", "10.0.1" },
+        };
+    }
+}
